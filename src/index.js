330badbfd68830630803f27edf919ca041c0665a
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-24 18:57:35 +0200

    add Header and three Button components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Header = () => {
+  return (
+      <h1>Give Feedback</h1>
+  )
+}
+
+const Button = () => {
+  return (
+
+      <button>Feedback Button</button>
+
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <div>Initial code</div>,
+  <App />,
   document.getElementById('root')
 )
