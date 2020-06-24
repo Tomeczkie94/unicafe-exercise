ac6f4a767da5e8385c4136ead9e88010e3bd8931
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-24 19:24:45 +0200

    add handleClick and text props to buttons

--- src/index.js
+++ src/index.js
@@ -8,21 +8,40 @@ const Header = () => {
   )
 }
 
-const Button = () => {
-  return (
-
-      <button>Feedback Button</button>
-
+const Button = (props) =>
+  (
+    <button onClick={props.handleClick}>{props.text}</button>
   )
-}
+
 
 const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGoodFeedback = (newFeedback) => {
+    setGood(newFeedback)
+  }
+
+  const addNeutralFeedback = (newFeedback) => {
+    setNeutral(newFeedback)
+  }
+
+  const addBadFeedback = (newFeedback) => {
+    setBad(newFeedback)
+  }
+
+
   return (
     <div>
       <Header />
-      <Button />
-      <Button />
-      <Button />
+      <Button handleClick= {() => addGoodFeedback(good + 1)} text='Good' />
+      <Button handleClick= {() => addNeutralFeedback(neutral + 1)} text='Neutral' />
+      <Button handleClick= {() => addBadFeedback(bad + 1)} text='Bad' />
+
+      <Header />
+      // This header will state: Statistics
     </div>
   )
 }
