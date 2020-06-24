81619e323f8f96be89db4ad56b8937e943f16479
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-24 21:20:43 +0200

    not working

--- src/index.js
+++ src/index.js
@@ -23,26 +23,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [points, setPoints] = useState(0)
   const all = good + neutral + bad
 
-  const addGoodFeedback = (newFeedback) => {
+  const addGoodFeedback = (newFeedback, pointsChange) => {
     setGood(newFeedback)
+    setPoints(pointsChange)
   }
 
   const addNeutralFeedback = (newFeedback) => {
     setNeutral(newFeedback)
   }
 
-  const addBadFeedback = (newFeedback) => {
+  const addBadFeedback = (newFeedback, pointsChange) => {
     setBad(newFeedback)
+    setPoints(pointsChange)
   }
 
   return (
     <div>
       <Header text='Give feedback' />
-      <Button handleClick= {() => addGoodFeedback(good + 1)} text='Good' />
+      <Button handleClick= {() => addGoodFeedback(good + 1, points + 1)} text='Good' />
       <Button handleClick= {() => addNeutralFeedback(neutral + 1)} text='Neutral' />
-      <Button handleClick= {() => addBadFeedback(bad + 1)} text='Bad' />
+      <Button handleClick= {() => addBadFeedback(bad + 1, points - 1)} text='Bad' />
 
       <Header text='Statistics' />
       <Display text="Good" /> {good}
