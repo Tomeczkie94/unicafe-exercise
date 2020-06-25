c8f7477765776cb7199f9e7fa7497a77062d8bea
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-25 21:07:18 +0200

    table works, percentage doesn't

--- src/index.js
+++ src/index.js
@@ -15,24 +15,33 @@ const Button = (props) =>
 
 const Statistic  = (props) => {
   return (
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
+}
+
+const Percenage = (props) => {
+  return (props.good/(props.good + props.neutral + props.bad)) * 100
 }
 
 const Statistics = (props) => {
 
-  if(props.good > 0 || props.neutral > 0 || props.bad > 0) {
+  if(props.good + props.neutral + props.bad !== 0) {
 
     return (
       <div>
-        <Statistic  text="Good" value= {props.good}/>
-        <Statistic  text="Neutral"value= {props.neutral}/>
-        <Statistic  text="Bad" value= {props.bad}/>
-        <Statistic  text="All" value= {props.good + props.neutral + props.bad} />
-        <Statistic  text="Average" value= {(props.good * 1) + (props.bad * -1)/(props.good + props.neutral + props.bad)} />
-        <p>Positive {(props.good/(props.good + props.neutral + props.bad)) * 100}% </p>
+        <table>
+          <tbody>
+            <Statistic  text="Good" value= {props.good}/>
+            <Statistic  text="Neutral"value= {props.neutral}/>
+            <Statistic  text="Bad" value= {props.bad}/>
+            <Statistic  text="All" value= {props.good + props.neutral + props.bad} />
+            <Statistic  text="Average" value= {(props.good * 1) + (props.bad * -1)/(props.good + props.neutral + props.bad)} />
+            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100}  />
+          </tbody>
+        </table>
       </div>
     )
   }
@@ -67,7 +76,6 @@ const App = () => {
       <Button handleClick= {addGoodFeedback} text='Good' />
       <Button handleClick= {addNeutralFeedback} text='Neutral' />
       <Button handleClick= {addBadFeedback} text='Bad' />
-
       <Header text='Statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
