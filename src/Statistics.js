14e0e369d7bb81953702f3664a028cdbc33be299
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-26 20:57:21 +0200

    export Statistics component

---
+++ src/Statistics.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Statistic from './Statistic.js'
+
+const Statistics = (props) => {
+
+  if(props.good + props.neutral + props.bad !== 0) {
+
+    return (
+      <div>
+        <table>
+          <tbody>
+            <Statistic  text="Good" value= {props.good}/>
+            <Statistic  text="Neutral"value= {props.neutral}/>
+            <Statistic  text="Bad" value= {props.bad}/>
+            <Statistic  text="All" value= {props.good + props.neutral + props.bad} />
+            <Statistic  text="Average" value= {(props.good * 1) + (props.bad * -1)/(props.good + props.neutral + props.bad)} />
+            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+  else {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+}
+
+export default Statistics;
--- src/index.js
+++ src/index.js
@@ -3,39 +3,8 @@ import ReactDOM from 'react-dom';
 import Header from './Header.js';
 import Button from './Button.js'
 import Statistic from './Statistic.js'
+import Statistics from './Statistics.js'
 import './index.css';
-
-
-
-
-
-
-
-const Statistics = (props) => {
-
-  if(props.good + props.neutral + props.bad !== 0) {
-
-    return (
-      <div>
-        <table>
-          <tbody>
-            <Statistic  text="Good" value= {props.good}/>
-            <Statistic  text="Neutral"value= {props.neutral}/>
-            <Statistic  text="Bad" value= {props.bad}/>
-            <Statistic  text="All" value= {props.good + props.neutral + props.bad} />
-            <Statistic  text="Average" value= {(props.good * 1) + (props.bad * -1)/(props.good + props.neutral + props.bad)} />
-            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-  else {
-    return (
-      <p>No feedback given</p>
-    )
-  }
-}
 
 const App = () => {
 
