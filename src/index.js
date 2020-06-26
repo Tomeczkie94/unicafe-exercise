9f123ae3ccbdbbc371f69585b41704ff991f04ed
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-26 21:03:30 +0200

    remove unnecessary imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
-import Header from './Header.js';
-import Button from './Button.js';
-import Statistic from './Statistic.js';
-import Statistics from './Statistics.js';
 import App from './App.js';
 import './index.css';
 
