7014fd6ce280aab1195cde8337bb5b082a3fc158
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-26 06:10:10 +0200

    percentage works

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const Statistics = (props) => {
             <Statistic  text="Bad" value= {props.bad}/>
             <Statistic  text="All" value= {props.good + props.neutral + props.bad} />
             <Statistic  text="Average" value= {(props.good * 1) + (props.bad * -1)/(props.good + props.neutral + props.bad)} />
-            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100}  />
+            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
           </tbody>
         </table>
       </div>
