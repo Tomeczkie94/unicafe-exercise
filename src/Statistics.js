41bb1c0f7722a05995ea77fb0721d83ba4b78875
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-29 18:44:10 +0200

    extract statement body in 7th line of Statistics.js into a variable and use it in conditional statement

--- src/Statistics.js
+++ src/Statistics.js
@@ -3,18 +3,19 @@ import Statistic from './Statistic.js'
 
 const Statistics = (props) => {
 
-  if(props.good + props.neutral + props.bad !== 0) {
+  const hasVotes = (props.good + props.neutral + props.bad !==0)
 
+  if(hasVotes) {
     return (
       <div>
         <table>
           <tbody>
-            <Statistic text="Good" value= {props.good}/>
-            <Statistic text="Neutral"value= {props.neutral}/>
-            <Statistic text="Bad" value= {props.bad}/>
-            <Statistic text="All" value= {props.good + props.neutral + props.bad} />
-            <Statistic text="Average" value= {((props.good * 1) + (props.bad * -1))/(props.good + props.neutral + props.bad)} />
-            <Statistic text="Positive" value= {(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
+            <Statistic text="Good" value={props.good}/>
+            <Statistic text="Neutral" value={props.neutral}/>
+            <Statistic text="Bad" value={props.bad}/>
+            <Statistic text="All" value={props.good + props.neutral + props.bad} />
+            <Statistic text="Average" value={((props.good * 1) + (props.bad * -1))/(props.good + props.neutral + props.bad)} />
+            <Statistic text="Positive" value={(props.good/(props.good + props.neutral + props.bad)) * 100 + '%'} />
           </tbody>
         </table>
       </div>
